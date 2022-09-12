f6dc216ef860bc112c166c834c97ef90291ec8b2
Rafaela-Sanchez <rafaelasanchezjahu@hotmail.com>
2022-09-12 12:26:33 -0300

    Incluindo config.php, mudanças em autoload e rotas

--- App/autoload.php
+++ App/autoload.php
@@ -1,21 +1,34 @@
 <?php
 
-spl_autoload_register(function($classe_buscada)
+spl_autoload_register(function ($classe_buscada)
 {
-    $arquivo_controller = "Controller/" . $classe_buscada . ".php";
+    echo "Foi buscar a classe: " . $classe_buscada;
+    echo "<hr />";
+
+    $arquivo = BASEDIR . '/' . $classe_buscada. '.php';
+
+    if(file_exists($arquivo)){
+        include $arquivo;
+    }
+    else
+        exit('Arquivo não encontrado. Arquivo: ' . $arquivo . "<br />");
+
+    //include '../' . $classe_buscada . "<br />";
+    
+    /*$arquivo_controller = "Controller/" . $classe_buscada . ".php";
     $arquivo_model = "Model/" . $classe_buscada . ".php";
     $arquivo_dao = "DAO/" . $classe_buscada . "php";
 
     if(file_exists($arquivo_controller))
         include $arquivo_controller;
+
     else if(file_exists($arquivo_model))
         include $arquivo_model;
+
     else if(file_exists($arquivo_dao))
         include $arquivo_dao;  
-
-    echo "Foi buscar a classe: " . $classe_buscada;
-    echo "<hr />";
-
+    */
+   
 });
 
 ?>
---
+++ App/rotas.php
@@ -0,0 +1,62 @@
+<?php
+
+use App\Controller\CategoriaProdutoController;
+use App\Controller\PessoaController; 
+use App\Controller\ProdutoController;
+
+$uri_parse = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+
+switch($uri_parse)
+{
+
+    //PESSOAS 
+
+    case '/pessoa':
+        PessoaController::index();
+    break;
+
+    case '/pessoa/form':
+        PessoaController::form();
+    break;
+
+    case '/pessoa/save':
+        PessoaController::save();
+    break;
+
+    case '/pessoa/delete':
+        PessoaController::delete();
+    break;
+    
+    // PRODUTOS
+    
+    case '/produto/formulario':
+        ProdutoController::form();
+    break;
+
+    case '/produto':
+        ProdutoController::index();
+    break;
+
+    case '/produto/delete':
+       ProdutoController::delete();
+    break;
+
+    case '/produto/save':
+        ProdutoController::save();
+    break;
+
+    //CATEGORIA
+
+    case '/categoria/form':
+        CategoriaProdutoController::form();
+    break;
+
+    case '/categoria/save':
+        CategoriaProdutoController::save();
+    break;
+    
+    default:
+        echo "erro 404";
+    break;
+}
